ddb696d59e41e0794021523442041b850e260d03
Sarang Barot <sarangbarot2579@gmail.com>
2026-09-21 12:52:04 +0530

    Login and register UI and styling scss

---
+++ frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router'
+
+const Register = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Enter username"/>
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Enter email address"/>
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter password"/>
+          </div>
+          <button className="button primary-button">Register</button>
+        </form>
+
+        <p> Already have an account? <Link to={"/login"}>Login</Link></p>
+      </div>
+    </main>
+  );
+}
+
+export default Register
